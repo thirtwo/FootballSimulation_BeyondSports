384bdf60a693562ac809e8f5d07df93be85b7515
thirtwo <thirtwo@protonmail.com>
2023-09-20 13:35:48 +0200

    Add GameConfig

---
+++ Assets/_FootballSimulation/Scripts/Data/ScriptableObjects/GameConfig.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu(fileName ="GameConfig",menuName ="Thirtwo/Settings/GameConfig")]
+public class GameConfig : ScriptableObject
+{
+    [Header("Simulation Settings")]
+    public TextAsset SimulationDataAsset;
+}
